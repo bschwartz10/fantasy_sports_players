56bf54dd019fd26e3180a51383aff4d0edbfc1b7
bschwartz10 <bschwartz92@gmail.com>
2018-04-02 14:31:30 -0400

    Rework name_brief logic for edge cases

--- app/serializers/player_serializer.rb
+++ app/serializers/player_serializer.rb
@@ -12,17 +12,41 @@ class PlayerSerializer < ActiveModel::Serializer
   def name_brief
     case object.sport
       when 'football'
-        "#{object.firstname[0]}. #{object.lastname}"
+        football_name_brief
       when 'basketball'
-        "#{object.firstname} #{object.lastname[0]}."
+        basketball_name_brief
       when 'baseball'
-        "#{object.firstname[0]}. #{object.lastname[0]}."
+        baseball_name_brief
+    end
+  end
+
+  def football_name_brief
+    "#{object.firstname[0]}. #{object.lastname}"
+  end
+
+  def basketball_name_brief
+    last_name = object.lastname.split(' ')
+    if last_name.length == 1
+      "#{object.firstname} #{object.lastname[0]}."
+    elsif last_name.length == 2 && last_name[-1] == 'Jr.'
+      "#{object.firstname} #{last_name[0][0]}."
+    elsif last_name.length == 2
+      "#{object.firstname} #{last_name[-1][0]}."
+    end
+  end
+
+  def baseball_name_brief
+    last_name = object.lastname.split(' ')
+    if last_name.length == 1
+      "#{object.firstname[0]}. #{object.lastname[0]}."
+    elsif last_name.length == 2
+      "#{object.firstname[0]}. #{last_name[-1][0]}."
     end
   end
 
   def average_position_age_diff
-    return "N/A" if object[:age] == nil || object.average_age_for_position(object[:position]) == nil
-    (object[:age] - object.average_age_for_position(object[:position])).round(1)
+    return "N/A" if object.age == nil || object.average_age_for_position(object.position) == nil
+    (object.age - object.average_age_for_position(object.position)).round(1)
   end
 
 end
