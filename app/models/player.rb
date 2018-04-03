dabb9dcb1ea7c036c45b5bb5cdc9327dabc1c1a2
bschwartz10 <bschwartz92@gmail.com>
2018-04-03 10:04:03 -0400

    get_all method now creates players with an attached position
    remove unused scope

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,17 +1,16 @@
 class Player < ApplicationRecord
   serialize :icons
-  scope :has_age_and_position, -> { where.not(age: nil).where.not(position: "") }
+  belongs_to :position
 
-   def self.get_all
-     PlayerService.get_players.map do |raw_player|
-       Player.create(raw_player)
-     end
-   end
-
-   def average_age_for_position(position)
-     age = Player.has_age_and_position.where(position: position).average(:age)
-     age.to_f if age
-   end
-
+  def self.get_all
+    CbsService.get_players.map do |raw_player|
+      Position.all.each do |position|
+        if position.name == raw_player[:position]
+          raw_player.delete(:position)
+          position.players.create(raw_player)
+        end
+      end
+    end
+  end
 
 end
